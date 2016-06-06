310e2cd9883f57086bb67e72b16bb5c3b834c28a
zgrasa <samuel.zgraggen@bbcag.ch>
2016-06-06 21:17:34 +0200

    Update and rename commentMapper.php to CommentMapper.php

---
+++ SSB/Model/CommentMapper.php
@@ -0,0 +1,78 @@
+<?php
+require_once __DIR__ . '/../Class/Comment.php';
+class CommentMapper
+{
+    private $file = null;
+    private $users = [];
+    
+    public function createCommentEntry($blogId, $userId, $comment)
+    {
+        $newId = $this->getNextId();
+        $this->comments[$newId] = new Comment($newId, $blogId, $userId, $comment, new DateTime());
+    }
+
+    private function getNextId()
+    {
+        end($this->comments);
+
+        return key($this->comments) + 1;
+    }
+
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    public function getComment($id)
+    {
+        return $this->comments[$id];
+    }
+
+    public function deleteComment($id)
+    {
+        unset($this->comments[$id]);
+    }
+    
+    function __construct()
+    {
+        $this->file = __DIR__ . '\SaveFiles\Comments.txt';
+        $comments = [];
+        $filecontent = file_get_contents($this->file);
+        preg_match_all('/(?P<id>\d*)§(?P<blogId>\d*)§(?P<userId>\d*)§(?P<comment>.*)§(?P<datetime>.*)eol/Us',
+            $filecontent, $comments, PREG_SET_ORDER);
+        foreach ($comments as $comment) {
+            $this->comments[$comment['id']] = new Comment($comment['id'], $comment['blogId'], $comment['userId'],
+                $comment['comment'],
+                new DateTime($comment['datetime']));
+        }
+    }
+    public function createComment($blogId, $userId, $comment)
+    {
+        $newId = $this->getNextId();
+        $this->users[$newId] = new User($newId, $mail, md5($pw));
+    }
+    private function getNextId()
+    {
+        end($this->users);
+        return key($this->users) + 1;
+    }
+    public function getUsers()
+    {
+        return $this->users;
+    }
+    function save()
+    {
+        $data = '';
+        foreach ($this->comments as $comment) {
+            if ($comment == null) {
+                continue;
+            }
+
+            $data .= $comment->getId() . '§' . $comment->getBlogId() . '§' . $comment->getUserId() . '§' . $comment->getComment() . '§' . $comment->getDatetime()->format('Y-m-d H:i:s') . "eol\n";
+        }
+
+        file_put_contents($this->file, $data);
+    }
+    
+    
+}
